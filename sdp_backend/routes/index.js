6aa1353eefb3cd8c702a793992bb6b15a8715268
Rachel-Mohlomi <1396431@students.wits.ac.za>
2018-08-29 00:16:31 +0200

    Working on  students csv

--- sdp_backend/routes/index.js
+++ sdp_backend/routes/index.js
@@ -105,6 +105,75 @@ router.post('/upload/courses', function(req, res){
    
 });
 
+router.post('/upload/students', function(req, res){
+  if(!req.files){
+      return res.status(400).send('No files were uploaded')
+  }
+
+  let std_csvFile = req.files.file
+
+  console.log(std_csvFile)
+
+  std_csvFile.mv(`./public/${std_csvFile.name}`,function(err){
+    if(err){
+        console.log(err)
+        return res.status(500).send(err);
+    }
+
+    var file = `./public/${std_csvFile.name}`;
+        console.log('Processing students file');
+        var parser = parse({delimiter: ','}, function (err, data) {
+       // note: array element at index 0 contains the row of headers that we should skip
+        console.log(data);
+        var dup_students = [];
+        for(var i=0;i<data.length;i++){
+            dup_students[i] = (data[i][0]).substring(0,8); // the array of students only
+        }
+        console.log("The students going to database");
+
+        students = [];
+        //removes duplicates
+        Array.prototype.unique= function (){
+          return this.reduce(function(previous, current, index, array)
+            {
+              previous[current.toString()+typeof(current)]=current;
+              return array.length-1 == index ? Object.keys(previous).reduce(function(prev,cur)
+                {
+                  prev.push(previous[cur]);
+                  return prev;
+                },[]) : previous;
+            }, {});
+        };
+
+    students = dup_students.unique();
+
+    var unique_students = [];
+
+    for(var i = 0; i<students.length;i++ ){
+      let arr = []
+      arr.push(students[i]);
+      unique_students.push(arr);
+    }
+
+    console.log(unique_students);
+
+        var sql = "INSERT INTO Registered (unique_students) VALUES ?";
+        connection.query(sql, [Students], function(err) {
+          if (err) throw err;
+          connection.end();
+           });
+
+  //add courses to database
+       });
+       fs.createReadStream(inputFile).pipe(parser);
+       res.send("Nayi i students nton nton")
+
+     
+
+  });
+  
+});
+
 
 
 

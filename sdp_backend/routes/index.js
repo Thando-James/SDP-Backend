da2a72a0c32c16031eea175bf27f6a39af4461fb
Thando-James <thandojames80@gmail.com>
2018-09-04 21:48:56 +0200

    a bit of error handling

--- sdp_backend/routes/index.js
+++ sdp_backend/routes/index.js
@@ -16,14 +16,18 @@ var connection = mysql.createConnection({
 connection.connect();
 
 router.get('/display/courses', function(req,res){
-    connection.query("SELECT DISTINCT Course_Code  FROM Registered LIMIT 10", function(err,results) {     
-        console.log('The courses are: ', results); 
-        if(err){
-          console.log(err)
-          return res.status(500).send(err);
-        }  
-        return res.json(results);    
-    });
+    try {
+        connection.query("SELECT DISTINCT Course_Code  FROM Registered LIMIT 10", function(err,results) {     
+            console.log('The courses are: ', results); 
+            if(err){
+              console.log(err)
+              return res.status(500).send(err);
+            }  
+            return res.json(results);    
+        });   
+    } catch (error) {
+        return res.json({errorType:'Database',errorMessage:error})
+    }
 });
 
 router.post('/generate', function(req,res){
@@ -39,16 +43,20 @@ router.post('/generate', function(req,res){
     // }else{
     //     console.log('The generate button passed the test :) ');
     // }
-
-
-      PythonShell.PythonShell.run('/GraphColouring.py', { args: [selected_courses,maxSessions,clashParameter]}, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        console.log("dgdgh")
-        results = JSON.parse(results)
-        console.log(results);
-        res.json(results)
-      });
+    try {
+        PythonShell.PythonShell.run('/GraphColouring.py', { args: [selected_courses,maxSessions,clashParameter]}, function (err, results) {
+            if (err){
+                return res.json({errorType:'Python Shell',errorMessage:err})
+            }
+            // results is an array consisting of messages collected during execution
+            console.log("dgdgh")
+            results = JSON.parse(results)
+            console.log(results);
+            res.json(results)
+          });   
+    } catch (error) {
+        return res.json({errorType:'Python Shell',errorMessage:error})
+    }
 });
 
 router.post('/upload/courses', function(req, res){
@@ -58,74 +66,78 @@ router.post('/upload/courses', function(req, res){
 
     let csvFile = req.files.file
 
-    csvFile.mv(`./public/${csvFile.name}`,function(err){
-        if(err){
-            console.log(err)
-            return res.status(500).send(err);
-        }
-
-
-
-        //testing upload courses csv 
-        function testCourses(){
-            var resp= 200;
-            if(res.statusCode != resp){
-                console.log("Upload courses failed the test");
+    try {
+        csvFile.mv(`./public/${csvFile.name}`,function(err){
+            if(err){
+                console.log(err)
+                return res.status(500).send(err);
             }
-            else{
-                console.log("Upload courses passed the test");
-            }
-        };
-
-
-        console.log('Testing upload courses: ');
-        testCourses();
-
-        //testing ends here
-
-
-        let inputFile =`./public/${csvFile.name}`;
-        console.log('Processing courses file');
-        let parser = parse({delimiter: '/'}, function (err, data) {
-            console.log(data);
-            let a = [];
-            for(let i=0;i<data.length;i++){
-                a[i] = (data[i][0]).substring(0,8);
-            } 
-            console.log("The courses going to database");
-            courses = [];
-
-            //removes duplicates
-            Array.prototype.unique= function (){
-                return this.reduce(function(previous, current, index, array){
-                    previous[current.toString()+typeof(current)]=current;
-                    return array.length-1 == index ? Object.keys(previous).reduce(function(prev,cur)
-                        {
-                            prev.push(previous[cur]);
-                            return prev;
-                        },[]) : previous;
-                }, {});
+    
+    
+    
+            //testing upload courses csv 
+            function testCourses(){
+                var resp= 200;
+                if(res.statusCode != resp){
+                    console.log("Upload courses failed the test");
+                }
+                else{
+                    console.log("Upload courses passed the test");
+                }
             };
-
-            courses = a.unique();
-            let course_codes = [];
-            for(let i = 0; i<courses.length;i++ ){
-              let arr = []
-              arr.push(courses[i]);
-              course_codes.push(arr);
-            }
-  
-            console.log(course_codes);
-
-            // let sql = "INSERT INTO Courses (course_code) VALUES ?";
-            // connection.query(sql, [course_codes], function(err) {
-            //     if (err) throw err;
-            // });
-        });
-
-        fs.createReadStream(inputFile).pipe(parser);
-        res.send("hfhgvvj")
-    });
+    
+    
+            console.log('Testing upload courses: ');
+            testCourses();
+    
+            //testing ends here
+    
+    
+            let inputFile =`./public/${csvFile.name}`;
+            console.log('Processing courses file');
+            let parser = parse({delimiter: '/'}, function (err, data) {
+                console.log(data);
+                let a = [];
+                for(let i=0;i<data.length;i++){
+                    a[i] = (data[i][0]).substring(0,8);
+                } 
+                console.log("The courses going to database");
+                courses = [];
+    
+                //removes duplicates
+                Array.prototype.unique= function (){
+                    return this.reduce(function(previous, current, index, array){
+                        previous[current.toString()+typeof(current)]=current;
+                        return array.length-1 == index ? Object.keys(previous).reduce(function(prev,cur)
+                            {
+                                prev.push(previous[cur]);
+                                return prev;
+                            },[]) : previous;
+                    }, {});
+                };
+    
+                courses = a.unique();
+                let course_codes = [];
+                for(let i = 0; i<courses.length;i++ ){
+                  let arr = []
+                  arr.push(courses[i]);
+                  course_codes.push(arr);
+                }
+      
+                console.log(course_codes);
+    
+                // let sql = "INSERT INTO Courses (course_code) VALUES ?";
+                // connection.query(sql, [course_codes], function(err) {
+                //     if (err) throw err;
+                // });
+            });
+    
+            fs.createReadStream(inputFile).pipe(parser);
+            res.send("hfhgvvj")
+        });   
+    } catch (error) {
+        return res.json({errorType:'csv',errorMessage:error})
+    }
 });
 
 router.post('/upload/students', function(req, res){
@@ -135,55 +147,59 @@ router.post('/upload/students', function(req, res){
 
     let csvFile = req.files.file
 
-    csvFile.mv(`./public/${csvFile.name}`,function(err){
-        if(err){
-            console.log(err)
-            return res.status(500).send(err);
-        }
-
-
-     //testing upload students csv 
-        function testStudents(){
-            var resp= 200;
-            if(res.statusCode != resp){
-                console.log("Upload students csv failed the test");
+    try {
+        csvFile.mv(`./public/${csvFile.name}`,function(err){
+            if(err){
+                console.log(err)
+                return res.status(500).send(err);
             }
-            else{
-                console.log("Upload students csv passed the test");
-            }
-        };
-
-
-        console.log('Testing upload students csv: ');
-        testStudents();
-
-        //testing ends here
-
-        let inputFile =`./public/${csvFile.name}`;
-        console.log('Processing students file');
-        let parser = parse({delimiter: '\n'}, function (err, data){
-            if (err) {
-                console.log('There was an error==> ', err)
-            }
-            big_arr = []
-            for(let i=0; i<data.length; i++){
-              let splitData = data[i][0].split(',');
-              let temp = [splitData[0], (splitData[4]).substring(0,8)];
-              big_arr.push(temp);
-            }
-
-            console.log('Students data going to database');
-            console.log(big_arr);
-
-            let sql = "INSERT INTO Registered (Std_ID, Course_Code) VALUES ?";
-            // connection.query(sql, [big_arr], function(err) {
-            //   if (err) throw err;
-            // });
-        });
-
-        fs.createReadStream(inputFile).pipe(parser);
-        res.send("hfhgvvj")
-    });
+    
+    
+         //testing upload students csv 
+            function testStudents(){
+                var resp= 200;
+                if(res.statusCode != resp){
+                    console.log("Upload students csv failed the test");
+                }
+                else{
+                    console.log("Upload students csv passed the test");
+                }
+            };
+    
+    
+            console.log('Testing upload students csv: ');
+            testStudents();
+    
+            //testing ends here
+    
+            let inputFile =`./public/${csvFile.name}`;
+            console.log('Processing students file');
+            let parser = parse({delimiter: '\n'}, function (err, data){
+                if (err) {
+                    console.log('There was an error==> ', err)
+                }
+                big_arr = []
+                for(let i=0; i<data.length; i++){
+                  let splitData = data[i][0].split(',');
+                  let temp = [splitData[0], (splitData[4]).substring(0,8)];
+                  big_arr.push(temp);
+                }
+    
+                console.log('Students data going to database');
+                console.log(big_arr);
+    
+                let sql = "INSERT INTO Registered (Std_ID, Course_Code) VALUES ?";
+                // connection.query(sql, [big_arr], function(err) {
+                //   if (err) throw err;
+                // });
+            });
+    
+            fs.createReadStream(inputFile).pipe(parser);
+            res.send("hfhgvvj")
+        });  
+    } catch (error) {
+        return res.json({errorType:'csv',errorMessage:error})
+    }   
 });
 
 

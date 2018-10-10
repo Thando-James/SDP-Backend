d50c7c8d5357b073c3b49ac140a6ea8e06ac3416
James Ndhlovu <james@naked.insure>
2018-10-10 21:15:48 +0200

    a

--- sdp_backend/routes/index.js
+++ sdp_backend/routes/index.js
@@ -222,8 +222,8 @@ router.post('/generate', function(req,res){
                         let obj = {
                             subject:results[a][b],
                             data : [date.format("LL"),date.format("YYYY-MM-DD")],
-                            start : date,
-                            end : date,
+                            start : newDate,
+                            end : newDate,
                             title : results[a][b],
                             allDay : false,
                             resource : [{session: a+1}]

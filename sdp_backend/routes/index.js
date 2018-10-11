71303e4c92283a19e7d95b0a7235a2db74d7668d
Nelly <nellygrattie@gmailcom>
2018-10-11 20:25:57 +0200

    edited student router

--- sdp_backend/routes/index.js
+++ sdp_backend/routes/index.js
@@ -169,6 +169,7 @@ try{
 
 router.post('/addStudent', function(req,res){
     let newStudent = req.body.newKid;
+    console.log("new students are: ", newStudent);
 try{
     let sql = "INSERT IGNORE INTO Registered (Std_ID,Course_Code) VALUES ?"
     connection.query(sql, [newStudent], function(err){

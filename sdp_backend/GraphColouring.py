2f32454e5b5f556a8fbc22c5ad77cfcab7a4369c
James Ndhlovu <james@naked.insure>
2018-11-24 20:36:47 +0200

    wo

--- sdp_backend/GraphColouring.py
+++ sdp_backend/GraphColouring.py
@@ -652,7 +652,7 @@ def permute(sessions):
     return result
 
    
-populationSize=100
+populationSize=10000
 def populate(session,populationSize):
     population=[]
     temp=session[:]
@@ -728,7 +728,7 @@ def Breed(parent1,parent2):
 #We take the top n organisms and discard the rest
 
 generation=0
-numGenerations=1
+numGenerations=10
 while generation < numGenerations:
   
     sortedArray=sorted(thePopulation,key=operator.itemgetter(len(thePopulation[0])-1))
